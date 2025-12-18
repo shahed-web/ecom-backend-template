353172ea577f52ff4352390cb06e48704d6a48f4
shahed-web <shahed.codeweb@gmail.com>
2025-12-18 18:11:33 +0600

    db connection done

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,7 +1,39 @@
 import {type Request, type Response} from 'express';
+import { prisma } from '../lib/prisma';
 
-export const login = async (req: Request, res: Response) => {
+interface AuthRequest {
+  name?: string;
+  email: string;
+  password: string;
+}
 
-    res.status(200).json({message: 'Login successful', data: req.body});    
-    res.end()
+interface AuthResponse {
+  message: string;
+  token?: string;
+}
+
+export const login = async (req: Request<{},{}, AuthRequest>, res: Response<AuthResponse>) => {
+    try {
+        const email: string = req.body.email;
+        const password: string = req.body.password;
+        if(!email || !password) {
+            res.status(400).json({message: 'Email and password are required'});
+            res.end()
+            return; 
+        }   
+        const user = await prisma.user.create({
+            data: {
+                email: email,  
+                password: password
+            }
+        });
+        res.status(200).json({message: 'Login successful'});    
+        res.end()
+        return 
+    }catch(error) {
+        console.error(error)
+        res.status(500).json({message: 'Internal server error'});
+        res.end()
+        return; 
+    }
 }
